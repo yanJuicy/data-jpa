faededf5c2d8c7c438ff6a1aa806258850d26453
yanJuicy <hanjub3@gmail.com>
2023-08-19 13:57:59 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,19 +5,41 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+	@PersistenceContext
+	private EntityManager em;
 
-    public Member save(Member member) {
-        em.persist(member);
-        return member;
-    }
+	public Member save(Member member) {
+		em.persist(member);
+		return member;
+	}
 
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+	public Member find(Long id) {
+		return em.find(Member.class, id);
+	}
+
+	public void delete(Member member) {
+		em.remove(member);
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+
+	public Optional<Member> findById(Long id) {
+		Member member = em.find(Member.class, id);
+		return Optional.ofNullable(member);
+	}
+
+	public long count() {
+		return em.createQuery("select count(m) from Member m", Long.class)
+				.getSingleResult();
+	}
 
 }
