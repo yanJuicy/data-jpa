9624b2eaa8eab07e9ab4cb03af7e7df819d27b74
yanJuicy <hanjub3@gmail.com>
2023-08-24 02:13:02 +0900

    @Query, 값, DTO 조회하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -16,4 +17,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT m FROM Member m WHERE m.username = :username AND m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("SELECT m.username FROM Member m")
+    List<String> findUsernameList();
+
+    @Query("SELECT new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "FROM Member m JOIN m.team t")
+    List<MemberDto> findMemberDto();
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -16,48 +18,66 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Rollback(value = false)
 class MemberRepositoryTest {
 
-	@Autowired
-	MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
-	@Test
-	void testMember() {
-		Member member = new Member("memberA");
-		Member savedMember = memberRepository.save(member);
+    @Autowired
+    TeamRepository teamRepository;
 
-		Member findMember = memberRepository.findById(savedMember.getId()).get();
+    @Test
+    void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
 
-		assertThat(findMember.getId()).isEqualTo(member.getId());
-		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
-		assertThat(findMember).isEqualTo(member);
-	}
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 
-	@Test
-	public void basicCRUD() {
-		Member member1 = new Member("member1");
-		Member member2 = new Member("member2");
-		memberRepository.save(member1);
-		memberRepository.save(member2);
+        assertThat(findMember).isEqualTo(member);
+    }
 
-		//단건 조회 검증
-		Member findMember1 = memberRepository.findById(member1.getId()).get();
-		Member findMember2 = memberRepository.findById(member2.getId()).get();
-		assertThat(findMember1).isEqualTo(member1);
-		assertThat(findMember2).isEqualTo(member2);
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
-		//리스트 조회 검증
-		List<Member> all = memberRepository.findAll();
-		assertThat(all.size()).isEqualTo(2);
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
 
-		//카운트 검증
-		long count = memberRepository.count();
-		assertThat(count).isEqualTo(2);
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
 
-		//삭제 검증
-		memberRepository.delete(member1);
-		memberRepository.delete(member2);
-		long deletedCount = memberRepository.count();
-		assertThat(deletedCount).isEqualTo(0);
-	}
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
 
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+    }
+
+    @Test
+    void findMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
+    }
+	
 }
