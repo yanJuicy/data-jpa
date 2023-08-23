b2bf884794bd1d9e58248c7ee624b20cc3f030af
yanJuicy <hanjub3@gmail.com>
2023-08-24 02:42:24 +0900

    파라미터 바인딩

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -24,4 +25,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("SELECT new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
             "FROM Member m JOIN m.team t")
     List<MemberDto> findMemberDto();
+
+    @Query("SELECT m FROM Member m WHERE m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    @Query("SELECT m FROM Member m WHERE m.username IN :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
 }
